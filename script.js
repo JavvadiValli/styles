4f586e80e750350400796a7bd977e6e9c93a3d70
Javvadi Durga Padma Nagavalli <135938371+JavvadiValli@users.noreply.github.com>
2023-07-19 18:41:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function bharatanatyam(){
+    document.getElementById("dances").src="https://tse4.mm.bing.net/th?id=OIP.99Z8J0ItBYCQ6MRF2HdNIwHaKr&pid=Api&P=0&h=180";
+}
+function western(){
+    document.getElementById("dances").src="https://tse3.mm.bing.net/th?id=OIP.O_e3zaVngIHyvUj1UYRoegHaEK&pid=Api&P=0&h=180";
+}
+function kathakali(){
+    document.getElementById("dances").src="https://tse1.mm.bing.net/th?id=OIP.i_IK48ezGLgerYMoJuRkiwHaE7&pid=Api&P=0&h=180";
+}
